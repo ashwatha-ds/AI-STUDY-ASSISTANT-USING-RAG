77c045d66aa09389e444e83296e1abea863ddfdd
ASHWATHA <ashuabi44@gmail.com>
2026-05-09 15:30:51 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- utils/pdf_processor.py
+++ utils/pdf_processor.py
@@ -1,5 +1,5 @@
 import fitz  # PyMuPDF
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 import io
 
